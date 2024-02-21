6d35a4ea521cc54b8e19949940ac5281705960f2
Giura Emanuel <emanuel@singulink.com>
2024-02-21 17:03:00 +0200

    Add ToString tests

---
+++ Tests/Singulink.Globalization.Currency.Tests/MoneyTests/ToStringTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Singulink.Globalization.Tests.MoneyTests;
+
+[TestClass]
+public class ToStringTests
+{
+    private static readonly CultureInfo _enUS = CultureInfo.GetCultureInfo("en-US");
+
+    [TestMethod]
+    public void ToString_DefaultFormat()
+    {
+        new Money(100m, "USD").ToString(null, null).ShouldBe("USD\u00A0100.00");
+    }
+
+    [TestMethod]
+    public void ToString_CultureDependentInternational_NumberWithGroupSeparators_DecimalsWithBankerRounding()
+    {
+        new Money(100m, "USD").ToString("CN$", _enUS).ShouldBe("USD\u00A0100.00");
+    }
+
+    [TestMethod]
+    public void ToString_InternationalCulture_DigitsWithNoSeparators_NoDecimals()
+    {
+        new Money(1000.001m, "USD").ToString("ID*", _enUS).ShouldBe("USD\u00A01000.001");
+    }
+
+    [TestMethod]
+    public void ToString_Local_NumberWithGroupSeparators_AwayFromZero()
+    {
+        new Money(1000.005m, "CAD").ToString("LDF", _enUS).ShouldBe("CAD\u00A01000.01");
+    }
+}
