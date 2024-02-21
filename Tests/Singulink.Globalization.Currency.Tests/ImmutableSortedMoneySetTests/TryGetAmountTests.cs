aeaf33aac8d8a511c12c1dca960a9dafd1acedc2
Giura Emanuel <emanuel@singulink.com>
2024-02-21 20:44:17 +0200

    add TryGetAmount tests for ImmutableSortedMoneySet and SortedMoneySet

---
+++ Tests/Singulink.Globalization.Currency.Tests/ImmutableSortedMoneySetTests/TryGetAmountTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Shouldly;
+
+namespace Singulink.Globalization.Tests.ImmutableSortedMoneySetTests;
+
+[TestClass]
+public class TryGetAmountTests
+{
+    private static readonly ImmutableSortedMoneySet _usd100Set = new ImmutableSortedMoneySet(new[] { new Money(100m, "USD") });
+
+    [TestMethod]
+    public void TryGetAmount_AmountExists_ReturnsTrue()
+    {
+        _usd100Set.TryGetAmount("USD", out var result).ShouldBeTrue();
+    }
+
+    [TestMethod]
+    public void TryGetAmount_AmountDoesNotExist_ReturnsFalse()
+    {
+        _usd100Set.TryGetAmount("EUR", out var result).ShouldBeFalse();
+    }
+
+    [TestMethod]
+    public void TryGetAmount_CurrencyDoesNotExist_ThrowsArgumentException()
+    {
+        Should.Throw<ArgumentException>(() => _usd100Set.TryGetAmount("AAA", out var result));
+    }
+}
---
+++ Tests/Singulink.Globalization.Currency.Tests/SortedMoneySetTests/TryGetAmountTests.cs
@@ -0,0 +1,27 @@
+﻿using Shouldly;
+
+namespace Singulink.Globalization.Tests.SortedMoneySetTests;
+
+[TestClass]
+public class TryGetAmountTests
+{
+    private readonly ImmutableSortedMoneySet _usd100Set = new(new[] { new Money(100m, "USD") });
+
+    [TestMethod]
+    public void TryGetAmount_AmountExists_ReturnsTrue()
+    {
+         _usd100Set.ToSet().TryGetAmount("USD", out var result).ShouldBeTrue();
+    }
+
+    [TestMethod]
+    public void TryGetAmount_AmountDoesNotExist_ReturnsFalse()
+    {
+         _usd100Set.ToSet().TryGetAmount("EUR", out var result).ShouldBeFalse();
+    }
+
+    [TestMethod]
+    public void TryGetAmount_CurrencyDoesNotExist_ThrowsArgumentException()
+    {
+        Should.Throw<ArgumentException>(() => _usd100Set.ToSet().TryGetAmount("AAA", out var result));
+    }
+}
