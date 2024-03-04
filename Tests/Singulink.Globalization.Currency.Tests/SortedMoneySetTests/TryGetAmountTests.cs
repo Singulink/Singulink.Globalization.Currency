a88dc8baa287cb97c62af51ce362ff300505aa58
giura.paul@singulink.com <giura.paul@singulink.com>
2024-03-04 18:15:39 +0200

    Add missing MutableSortedSet.TryGetAmount tests

--- Tests/Singulink.Globalization.Currency.Tests/SortedMoneySetTests/TryGetAmountTests.cs
+++ Tests/Singulink.Globalization.Currency.Tests/SortedMoneySetTests/TryGetAmountTests.cs
@@ -12,8 +12,10 @@ public class TryGetAmountTests
 
     private readonly SortedMoneySet _set = ImmutableSet.ToSet();
 
+    // public void TryGetAmount(string currencyCode, out decimal amount) tests
+
     [TestMethod]
-    public void AmountExists_ReturnsTrueAndOutputsAmount()
+    public void GetByCurrencyCode_CurrencyExists_ReturnsTrueAndOutputsAmount()
     {
         _set.TryGetAmount("USD", out decimal amount).ShouldBeTrue();
         amount.ShouldBe(100m);
@@ -26,14 +28,41 @@ public class TryGetAmountTests
     }
 
     [TestMethod]
-    public void AmountDoesNotExist_ReturnsFalse()
+    public void GetByCurrencyCode_CurrencyDoesNotExist_ReturnsFalse()
     {
         _set.TryGetAmount("GBP", out _).ShouldBeFalse();
     }
 
     [TestMethod]
-    public void CurrencyDisallowed_ThrowsArgumentException()
+    public void GetByCurrencyCode_CurrencyDisallowed_ThrowsArgumentException()
     {
         Should.Throw<ArgumentException>(() => _set.TryGetAmount("AAA", out _));
+    }
+
+    // public void TryGetAmount(Currency currency, out decimal amount) tests
+
+    [TestMethod]
+    public void GetByCurrency_CurrencyExists_ReturnsTrueAndOutputsAmount()
+    {
+        _set.TryGetAmount(Currency.Get("USD"), out decimal amount).ShouldBeTrue();
+        amount.ShouldBe(100m);
+
+        _set.TryGetAmount(Currency.Get("CAD"), out amount).ShouldBeTrue();
+        amount.ShouldBe(50m);
+
+        _set.TryGetAmount(Currency.Get("EUR"), out amount).ShouldBeTrue();
+        amount.ShouldBe(25m);
+    }
+
+    [TestMethod]
+    public void GetByCurrency_CurrencyDoesNotExist_ReturnsFalse()
+    {
+        _set.TryGetAmount(Currency.Get("GBP"), out _).ShouldBeFalse();
+    }
+
+    [TestMethod]
+    public void GetByCurrency_CurrencyDisallowed_ThrowsArgumentException()
+    {
+        Should.Throw<ArgumentException>(() => _set.TryGetAmount(Currency.Get("AAA"), out _));
     }
 }
