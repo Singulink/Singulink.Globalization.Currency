344ae86e1df58cb03f646896077753c1781b66fe
giura.paul@singulink.com <giura.paul@singulink.com>
2024-03-05 20:14:02 +0200

    Add ImmutableSortedMoneySet.TransformAmounts test and optimize impl
    The set now returns itself if there are no changes.

---
+++ Tests/Singulink.Globalization.Currency.Tests/ImmutableSortedMoneySetTests/TransformAmountsTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace Singulink.Globalization.Tests.ImmutableSortedMoneySetTests;
+
+[TestClass]
+public class TransformAmountsTests
+{
+    private static readonly Money Usd100 = new(100m, "USD");
+    private static readonly Money Cad50 = new(50m, "CAD");
+    private static readonly Money Eur25 = new(25m, "EUR");
+    private static readonly ImmutableSortedMoneySet Set = [Usd100, Cad50, Eur25];
+
+    [TestMethod]
+    public void AllAmountsTransformed_UpdatesValues()
+    {
+        var resultSet = Set.TransformAmounts(x => x * 2);
+        resultSet.Count.ShouldBe(3);
+        resultSet.ShouldBe([new(200m, "USD"), new(100m, "CAD"), new(50m, "EUR")]);
+    }
+
+    [TestMethod]
+    public void IdentityTransform_NoChange()
+    {
+        var resultSet = Set.TransformAmounts(x => x);
+        resultSet.ShouldBeSameAs(Set);
+    }
+
+    [TestMethod]
+    public void EmptySet_NoChange()
+    {
+        ImmutableSortedMoneySet emptySet = [];
+        var resultSet = emptySet.TransformAmounts(x => x * 2);
+        resultSet.ShouldBeSameAs(emptySet);
+    }
+}
