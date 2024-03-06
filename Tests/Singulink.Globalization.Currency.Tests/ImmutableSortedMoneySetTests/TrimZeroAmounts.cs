a0a919a87276041e7a4cd482ea0180fd12b6468f
Mike Marynowski <mikem@singulink.com>
2024-03-06 05:34:53 -0500

    Add test to ImmutableSortedMoneySet.TrimZeroAmounts + cleanup

--- Tests/Singulink.Globalization.Currency.Tests/ImmutableSortedMoneySetTests/TrimZeroAmounts.cs
+++ Tests/Singulink.Globalization.Currency.Tests/ImmutableSortedMoneySetTests/TrimZeroAmounts.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PrefixClassName.MsTest;
+﻿using PrefixClassName.MsTest;
 using Shouldly;
 
 namespace Singulink.Globalization.Tests.ImmutableSortedMoneySetTests;
@@ -12,21 +11,31 @@ public class TrimZeroAmounts
     private static readonly Money Eur25 = new(25m, "EUR");
     private static readonly Money Gbp0 = new(0m, "GBP");
     private static readonly Money Jpy0 = new(0m, "JPY");
-    private static readonly ImmutableSortedMoneySet ZeroAmountsSet = [Usd100, Cad50, Eur25, Gbp0, Jpy0];
+
+    private static readonly ImmutableSortedMoneySet AllAmountsSet = [Usd100, Cad50, Gbp0, Eur25, Jpy0];
     private static readonly ImmutableSortedMoneySet NoZeroAmountsSet = [Usd100, Cad50, Eur25];
+    private static readonly ImmutableSortedMoneySet AllZeroAmountsSet = [Gbp0, Jpy0];
 
     [TestMethod]
-    public void TrimZeroAmounts_ZeroAmountsExist_RemovesZeroAmounts()
+    public void SomeZeroAmounts_RemovesZeroAmountValues()
     {
-        var resultSet = ZeroAmountsSet.TrimZeroAmounts();
+        var resultSet = AllAmountsSet.TrimZeroAmounts();
         resultSet.Count.ShouldBe(3);
-        resultSet.ShouldBe([Usd100, Cad50, Eur25]);
+        resultSet.ShouldBe(NoZeroAmountsSet);
     }
 
     [TestMethod]
-    public void TrimZeroAmounts_NoZeroAmountsExist_NoChange()
+    public void NoZeroAmounts_NoChange()
     {
         var resultSet = NoZeroAmountsSet.TrimZeroAmounts();
         resultSet.ShouldBeSameAs(NoZeroAmountsSet);
+    }
+
+    [TestMethod]
+    public void AllZeroAmounts_RemovesAllValues()
+    {
+        var resultSet = AllZeroAmountsSet.TrimZeroAmounts();
+        resultSet.Count.ShouldBe(0);
+        resultSet.ShouldBe([]);
     }
 }
