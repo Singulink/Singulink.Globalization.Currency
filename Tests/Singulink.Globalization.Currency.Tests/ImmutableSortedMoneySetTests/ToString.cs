9da7dd6d8a82243926d9a65d782b9ff8f285944e
Mike Marynowski <mikem@singulink.com>
2024-03-07 01:27:31 -0500

    Add ToString tests for sets

---
+++ Tests/Singulink.Globalization.Currency.Tests/ImmutableSortedMoneySetTests/ToString.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+namespace Singulink.Globalization.Tests.ImmutableSortedMoneySetTests;
+
+[PrefixTestClass]
+public class ToString
+{
+    private const char Sp = '\u00A0'; // The space char that ToString uses (non-breaking space)
+
+    private static readonly CultureInfo EnUS = CultureInfo.GetCultureInfo("en-US");
+    private static readonly CultureInfo FrFR = CultureInfo.GetCultureInfo("fe-FR");
+    private static readonly ImmutableSortedMoneySet Set = [Money.Create(1.23m, "USD"), Money.Create(0m, "CAD"), Money.Create(500m, "JPY")];
+
+    [TestMethod]
+    public void DefaultFormat()
+    {
+        CultureInfo.CurrentCulture = EnUS;
+
+        Set.ToString().ShouldBe($"CAD{Sp}0.00, JPY{Sp}500, USD{Sp}1.23");
+        Set.ToString(null, FrFR).ShouldBe($"0.00{Sp}CAD, 500{Sp}JPY, 1.23{Sp}USD");
+    }
+
+    [TestMethod]
+    public void ZeroAmountsIgnoredFormat()
+    {
+        CultureInfo.CurrentCulture = EnUS;
+
+        Set.ToString("!").ShouldBe($"JPY{Sp}500, USD{Sp}1.23");
+        Set.ToString("!", FrFR).ShouldBe($"500{Sp}JPY, 1.23{Sp}USD");
+    }
+
+    [TestMethod]
+    public void CustomMoneyFormat()
+    {
+        CultureInfo.CurrentCulture = EnUS;
+
+        Set.ToString("SN$0").ShouldBe("$0, ￥500, $1");
+    }
+
+    [TestMethod]
+    public void CustomMoneyWithZeroAmountsIgnoredFormat()
+    {
+        CultureInfo.CurrentCulture = EnUS;
+
+        Set.ToString("!SN$0").ShouldBe("￥500, $1");
+    }
+}
---
+++ Tests/Singulink.Globalization.Currency.Tests/SortedMoneySetTests/ToString.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace Singulink.Globalization.Tests.SortedMoneySetTests;
+
+[PrefixTestClass]
+public class ToString
+{
+    private const char Sp = '\u00A0'; // The space char that ToString uses (non-breaking space)
+
+    private static readonly CultureInfo EnUS = CultureInfo.GetCultureInfo("en-US");
+    private static readonly CultureInfo FrFR = CultureInfo.GetCultureInfo("fe-FR");
+    private static readonly ImmutableSortedMoneySet Set = [Money.Create(1.23m, "USD"), Money.Create(0m, "CAD"), Money.Create(500m, "JPY")];
+
+    private readonly SortedMoneySet _set = Set.ToSet();
+
+    [TestMethod]
+    public void DefaultFormat()
+    {
+        CultureInfo.CurrentCulture = EnUS;
+
+        _set.ToString().ShouldBe($"CAD{Sp}0.00, JPY{Sp}500, USD{Sp}1.23");
+        _set.ToString(null, FrFR).ShouldBe($"0.00{Sp}CAD, 500{Sp}JPY, 1.23{Sp}USD");
+    }
+
+    [TestMethod]
+    public void ZeroAmountsIgnoredFormat()
+    {
+        CultureInfo.CurrentCulture = EnUS;
+
+        _set.ToString("!").ShouldBe($"JPY{Sp}500, USD{Sp}1.23");
+        _set.ToString("!", FrFR).ShouldBe($"500{Sp}JPY, 1.23{Sp}USD");
+    }
+
+    [TestMethod]
+    public void CustomMoneyFormat()
+    {
+        CultureInfo.CurrentCulture = EnUS;
+
+        _set.ToString("SN$0").ShouldBe("$0, ￥500, $1");
+    }
+
+    [TestMethod]
+    public void CustomMoneyWithZeroAmountsIgnoredFormat()
+    {
+        CultureInfo.CurrentCulture = EnUS;
+
+        _set.ToString("!SN$0").ShouldBe("￥500, $1");
+    }
+}
