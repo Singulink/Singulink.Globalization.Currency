ca2aa5f81d587766891e51a8d7f34ea5513d2029
Mike Marynowski <mikem@singulink.com>
2024-03-07 21:12:30 -0500

    Test RoundToCurrencyDigits for both immutable sets

--- Tests/Singulink.Globalization.Currency.Tests/MoneySetTests/RoundToCurrencyDigits.Immutable.cs
+++ Tests/Singulink.Globalization.Currency.Tests/MoneySetTests/RoundToCurrencyDigits.Immutable.cs
@@ -11,21 +11,21 @@ public static partial class RoundToCurrencyDigits
     public class Immutable<T> where T : IImmutableMoneySet
     {
 #pragma warning disable SA1025 // Code should not contain multiple whitespace in a row
-        private static readonly ImmutableSortedMoneySet RoundDownResults = [new(10.000m, "USD"), new(6.0m, "JPY")];
-        private static readonly ImmutableSortedMoneySet RoundDownValues  = [new(10.004m, "USD"), new(6.2m, "JPY")];
-        private static readonly ImmutableSortedMoneySet MidpointValues   = [new(10.005m, "USD"), new(6.5m, "JPY")];
-        private static readonly ImmutableSortedMoneySet RoundUpValues    = [new(10.006m, "USD"), new(6.7m, "JPY")];
-        private static readonly ImmutableSortedMoneySet RoundUpResults   = [new(10.010m, "USD"), new(7.0m, "JPY")];
+        private static readonly IImmutableMoneySet RoundDownResults = T.Create(CurrencyRegistry.Default, [new(10.000m, "USD"), new(6.0m, "JPY")]);
+        private static readonly IImmutableMoneySet RoundDownValues  = T.Create(CurrencyRegistry.Default, [new(10.004m, "USD"), new(6.2m, "JPY")]);
+        private static readonly IImmutableMoneySet MidpointValues   = T.Create(CurrencyRegistry.Default, [new(10.005m, "USD"), new(6.5m, "JPY")]);
+        private static readonly IImmutableMoneySet RoundUpValues    = T.Create(CurrencyRegistry.Default, [new(10.006m, "USD"), new(6.7m, "JPY")]);
+        private static readonly IImmutableMoneySet RoundUpResults   = T.Create(CurrencyRegistry.Default, [new(10.010m, "USD"), new(7.0m, "JPY")]);
 #pragma warning restore SA1025
 
         [TestMethod]
         public void DefaultToEven()
         {
-            RoundDownResults.RoundToCurrencyDigits().ShouldBe(RoundDownResults, ignoreOrder: true);
+            RoundDownResults.RoundToCurrencyDigits().ShouldBeSameAs(RoundDownResults);
             RoundDownValues.RoundToCurrencyDigits().ShouldBe(RoundDownResults, ignoreOrder: true);
             MidpointValues.RoundToCurrencyDigits().ShouldBe(RoundDownResults, ignoreOrder: true);
             RoundUpValues.RoundToCurrencyDigits().ShouldBe(RoundUpResults, ignoreOrder: true);
-            RoundUpResults.RoundToCurrencyDigits().ShouldBe(RoundUpResults, ignoreOrder: true);
+            RoundUpResults.RoundToCurrencyDigits().ShouldBeSameAs(RoundUpResults);
         }
 
         [TestMethod]
@@ -33,11 +33,11 @@ public static partial class RoundToCurrencyDigits
         {
             const MidpointRounding mode = MidpointRounding.AwayFromZero;
 
-            RoundDownResults.RoundToCurrencyDigits(mode).ShouldBe(RoundDownResults, ignoreOrder: true);
+            RoundDownResults.RoundToCurrencyDigits(mode).ShouldBeSameAs(RoundDownResults);
             RoundDownValues.RoundToCurrencyDigits(mode).ShouldBe(RoundDownResults, ignoreOrder: true);
             MidpointValues.RoundToCurrencyDigits(mode).ShouldBe(RoundUpResults, ignoreOrder: true);
             RoundUpValues.RoundToCurrencyDigits(mode).ShouldBe(RoundUpResults, ignoreOrder: true);
-            RoundUpResults.RoundToCurrencyDigits(mode).ShouldBe(RoundUpResults, ignoreOrder: true);
+            RoundUpResults.RoundToCurrencyDigits(mode).ShouldBeSameAs(RoundUpResults);
         }
     }
 }
